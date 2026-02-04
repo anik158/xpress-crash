00116444de9edaacb0b5d5e5aab90b62b1f82ca2
Ahsan <ahsanan@outlook.com>
2026-02-05 00:11:52 +0600

    Query string(req.query)

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 const app = express(); 
 
+const port = process.env.PORT || 5001;
+
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public','index.html'));
 // });
@@ -17,14 +19,31 @@ const app = express();
 // app.use(express.static(path.join(__dirname,'public')))
 
 const posts = [
-  { title: 'Post 1', content: 'This is the content of post 1' },
-  { title: 'Post 2', content: 'This is the content of post 2' },
-  { title: 'Post 3', content: 'This is the content of post 3' },
+  { id: 1,title: 'Post 1', content: 'This is the content of post 1' },
+  { id: 2,title: 'Post 2', content: 'This is the content of post 2' },
+  { id: 3,title: 'Post 3', content: 'This is the content of post 3' },
 ];
 
+
+//  Get all posts
 app.get('/api/posts', (req, res) => {
-    res.json(posts);
+
+    const limit = parseInt(req.query.limit);
+
+    if(!isNaN(limit) && limit > 0){
+        return res.json(posts.slice(0, limit));
+    }else{
+        return res.json(posts);
+    }
 })
+
+// Get a specific post
+app.get('/api/posts/:id', (req, res) => {
+    const postId = parseInt(req.params.id);
+    const post = posts.filter(p => p.id === postId);
+
+    res.json(post);
+});
 
 app.listen(5050, () => {
   console.log('Server is running on port 5050');
