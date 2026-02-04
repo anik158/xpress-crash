20a8a8bc4d0623babbce4142af6404df9b9f6958
Ahsan <ahsanan@outlook.com>
2026-02-04 14:57:47 +0600

    postman utility

--- server.js
+++ server.js
@@ -14,8 +14,17 @@ const app = express();
 // });
 
 // static web server
-app.use(express.static(path.join(__dirname,'public')))
+// app.use(express.static(path.join(__dirname,'public')))
 
+const posts = [
+  { title: 'Post 1', content: 'This is the content of post 1' },
+  { title: 'Post 2', content: 'This is the content of post 2' },
+  { title: 'Post 3', content: 'This is the content of post 3' },
+];
+
+app.get('/api/posts', (req, res) => {
+    res.json(posts);
+})
 
 app.listen(5050, () => {
   console.log('Server is running on port 5050');
