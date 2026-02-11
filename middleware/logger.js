8eff5da6ee09c1e54946fd55896a1b1a8f3a708e
Ahsan <ahsanan@outlook.com>
2026-02-11 12:26:46 +0600

    color method un commented out

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,12 +2,12 @@ import colors from 'colors';
 
 const logger = (req, res, next) => {
 
-    // const colorMethod = {
-    //     GET: "blue",
-    //     POST: "green",
-    //     PUT: "yellow",
-    //     DELETE: "red",
-    // }
+    const colorMethod = {
+        GET: "blue",
+        POST: "green",
+        PUT: "yellow",
+        DELETE: "red",
+    }
     let color = colorMethod[req.method] || "white";
     console.log(colors[color](`${req.method} ${req.protocol}://${req.headers.host}${req.originalUrl}`));    next();
 }
